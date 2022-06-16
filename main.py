c0920a14ea3020c514f73399649985184d987633
dyson1357 <dyson1357@naver.com>
2022-06-16 17:37:01 +0900

    2.3까지 수강 완료

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import requests
+from bs4 import BeautifulSoup
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+indeed_result = requests.get("https://www.indeed.com/jobs?q=python&limit=50")
 
+indeed_soup = BeautifulSoup(indeed_result.text, "html.parser")
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+pagination = indeed_soup.find("div", {"class": "pagination"})
 
+pages = pagination.find_all('a')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+for page in pages:
+    print(page.find("span"))
