5dd4a5e562e3f2bce29af32d2a76f2152ad05daf
dyson1357 <dyson1357@naver.com>
2022-06-28 17:49:50 +0900

    네이버 검색 API 써보기

--- main.py
+++ main.py
@@ -1,20 +1,46 @@
-# 네이버 검색 API예제는 블로그를 비롯 전문자료까지 호출방법이 동일하므로 blog검색만 대표로 예제를 올렸습니다.
-# 네이버 검색 Open API 예제 - 블로그 검색
-import os
-import sys
+from selenium import webdriver
+from urllib.request import urlopen
+from selenium.webdriver.common.keys import Keys
+import time
 import urllib.request
-client_id = "dkSOkQneZv9U1uX34vEe"
-client_secret = "6Wr06H_Luk"
-encText = urllib.parse.quote("오늘")
-url = "https://openapi.naver.com/v1/search/image?query=" + encText # json 결과
-# url = "https://openapi.naver.com/v1/search/blog.xml?query=" + encText # xml 결과
-request = urllib.request.Request(url)
-request.add_header("X-Naver-Client-Id",client_id)
-request.add_header("X-Naver-Client-Secret",client_secret)
-response = urllib.request.urlopen(request)
-rescode = response.getcode()
-if(rescode==200):
-    response_body = response.read()
-    print(response_body.decode('utf-8'))
-else:
-    print("Error Code:" + rescode)
+
+driver = webdriver.Chrome('C:/chrome/chromedriver.exe')  # 여기에 크롬드라이브 다운로드 받은 경로를 입력한다.
+driver.get("https://www.google.co.kr/imghp?hl=ko&ogbl")
+elem = driver.find_element_by_name("q")
+elem.send_keys("한수환 총장")
+#엔터를 침
+elem.send_keys(Keys.RETURN)
+
+SCROLL_PAUSE_TIME = 1
+#스크롤 높이 구함
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    #브라우저 끝까지 스크롤 내림
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    #페이지 로드 기다려
+    time.sleep(SCROLL_PAUSE_TIME)
+
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        try:
+            driver.find_element_by_css_selector(".mye4qd").click()
+        except:
+            break
+    last_height = new_height
+
+images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd") #여러개의 이미지 선택하기, 클래스 이름 기준
+count = 1
+for image in images:
+    try:
+        image.click()
+        time.sleep(2)
+        imgUrl = driver.find_element_by_xpath(
+            '/html/body/div[2]/c-wiz/div[3]/div[2]/div[3]/div/div/div[3]/div[2]/c-wiz/div/div[1]/div[1]/div/div[2]/a/img').get_attribute(
+            "src")
+        urllib.request.urlretrieve(imgUrl, "./test/" + str(count) + ".txt")
+        count = count + 1
+    except:
+        pass
+
+driver.close()
