e23b378bb623e1bb37ae31ba3a1a196147b5dbf9
dyson1357 <dyson1357@naver.com>
2022-07-20 10:45:40 +0900

    최종 완성

--- get_Gmarket.py
+++ get_Gmarket.py
@@ -6,7 +6,6 @@ from datetime import timedelta
 import csv
 import time
 import re
-import math
 
 
 options = webdriver.ChromeOptions()
@@ -16,6 +15,7 @@ options.add_argument("headless")
 search_txt = input('Gmarket 검색 키워드: ')
 num_of_req = int(input('가져올 상품 데이터 수: '))
 
+#  시작 시간
 start = time.time()
 
 #  chromedriver 설정, 4.0부터는 아래와 같이 써야 함
@@ -30,15 +30,10 @@ driver.find_element(By.XPATH,
 driver.find_element(By.XPATH,
                     '//*[@id="region__content-status-information"]/div/div/div[2]/div[1]/div[2]/ul/li[2]').click()
 
-#  전체 상품 수
-total_item = driver.find_element(By.CLASS_NAME, 'text__item-count').text
-total_item = int(re.sub(r'\D', '', total_item))
-
 item_count = 1
 page = 1
 pList = []
 link_list = []
-total_page = math.ceil(total_item / 100)
 num = 1
 
 #  전체 페이지 순회
@@ -48,6 +43,7 @@ while item_count <= num_of_req * 2:
     links = driver.find_elements(By.CLASS_NAME, 'link__item')
 
     for i in links:
+        #  상세 페이지 링크 수집을 위한 a 태그의 href 속성 추출
         num += 1
         link_list.append(i.get_attribute('href'))
         link_list = list(dict.fromkeys(link_list))
@@ -57,9 +53,9 @@ while item_count <= num_of_req * 2:
         if item_count == num_of_req * 2:
             break
 
+        #  페이지 넘기는 작업 수행
         if (item_count % 200) == 0:
             next_btn = driver.find_element(By.CLASS_NAME, 'link__page-next')
-            #  페이지 넘기는 작업 수행
             if next_btn is None:
                 print("마지막 페이지까지 완료")
             else:
@@ -71,10 +67,11 @@ while item_count <= num_of_req * 2:
         break
 
 prod_count = 1
+#  수집된 링크 개수 확인(총 수집 요청 상품 수와 일치하는지 확인하기 위해)
 print(len(link_list))
 
 for product in link_list:
-
+    #  상품 상세 페이지 접속
     driver.get(product)
     driver.implicitly_wait(10)
 
@@ -88,16 +85,17 @@ for product in link_list:
 
     #  브랜드 정보 수집
     driver.find_element(By.CSS_SELECTOR,
-                        '#vip-tab_detail > div.vip-detailarea_productinfo.box__product-notice.js-toggle-content > div.box__product-notice-more > button').send_keys(
-        Keys.ENTER)
-    brand_search = getattr(driver.find_element(By.CSS_SELECTOR,
-                                        '#vip-tab_detail > div.vip-detailarea_productinfo.box__product-notice.js-toggle-content.on > div.box__product-notice-list > table:nth-child(1) > tbody > tr:nth-child(7) > th'),
-                    'text', None)
+                        '#vip-tab_detail > div.vip-detailarea_productinfo.box__product-notice.js-toggle-content > '
+                        'div.box__product-notice-more > button').send_keys(Keys.ENTER)
+    brand_search = getattr(driver.find_element(
+        By.CSS_SELECTOR, '#vip-tab_detail > div.vip-detailarea_productinfo.box__product-notice.js-toggle-content.on > '
+                         'div.box__product-notice-list > table:nth-child(1) > tbody > tr:nth-child(7) > th'), 'text', None)
 
     if brand_search == "브랜드":
-        brand = getattr(driver.find_element(By.CSS_SELECTOR,
-                                        '#vip-tab_detail > div.vip-detailarea_productinfo.box__product-notice.js-toggle-content.on > div.box__product-notice-list > table:nth-child(1) > tbody > tr:nth-child(7) > td'),
-                    'text', None)
+        brand = getattr(driver.find_element(
+            By.CSS_SELECTOR, '#vip-tab_detail > div.vip-detailarea_productinfo.box__product-notice.'
+                             'js-toggle-content.on > div.box__product-notice-list > table:nth-child(1) > tbody > '
+                             'tr:nth-child(7) > td'), 'text', None)
     else:
         brand = "정보 없음"
 
@@ -105,6 +103,7 @@ for product in link_list:
         brand = "정보 없음"
     brand = re.sub(r"^\s+|\s+$", "", brand)
 
+    #  수집된 정보들 리스트에 추가
     pList.append([name, price, brand])
     print(str(prod_count) + " | 상품명: " + name + " / 가격: " + price + " / 브랜드: " + brand)
     prod_count += 1
@@ -120,9 +119,8 @@ def saveToFile(filename, list):
 
 #  pList = set(map(tuple, pList))
 saveToFile(search_txt + '.csv', pList)
-
 driver.quit()
 
+#  종료 시간
 end = time.time()
-
 print("총 소요 시간(hh:mm:ss): ", timedelta(seconds=end-start))
